99d7e81450ee42819f6d7f8c1df0315f8ba0bbec
HEMANTH KUMAR SANGAM <109502503+Hemanthkumarsangam@users.noreply.github.com>
2024-08-20 23:14:02 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,7 +7,7 @@ function About() {
     <>
       <div className='about' id='about'>
         <img src={image} alt="" />
-        <p> I am a driven and innovative final-year Computer Science Engineering student with a strong interest in web development. 
+        <p> I am a self driven and innovative final-year Computer Science Engineering student with a strong interest in web development. 
           I am eager to launch my career in a dynamic and rewarding environment where I can leverage my academic background,
           problem-solving abilities, and creative thinking to contribute to organizational growth and success. </p>
       </div>
